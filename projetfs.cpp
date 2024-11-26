b7f6e9f876d1a50b1a09faae6a36de87f5aad025
khadijaDev <khadijaoukhlift@gmail.com>
2024-11-26 11:53:00 +0100

    changer scanf avec fgets

--- projetfs.cpp
+++ projetfs.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+//test
 #define max 50
    typedef struct {
    	int jour;
@@ -14,19 +14,26 @@
     int priorite; // plus importante ou moins
     DateEcheance date;
    }Tache; 
-   int taille=0;
+   //int tai=0;
    Tache T[50]; //Tableau des taches
-   int nbretache=0;
+   int nbretaches=0;
    int high;
    int low;
    //Ajout des taches
    void Ajoutertache(){
+   	int nbreTaches;
+   	printf("Ajoutter une tache\n");
    printf("Entrer le titre :\n ");
-   scanf("%s",T[taille].titre);
-   printf("Entrer la discription :\n");
-   scanf("%s",T[taille].description);
+   getchar();
+   fgets(T[nbreTaches].titre,sizeof(T[nbreTaches].titre),stdin);
+     // ([nbreTaches].titre[strcspn(titre,"\n")]=\'0');
+   //scanf("%s",T[taille].titre);
+   printf("Entrer la description :\n");
+      fgets(T[nbreTaches].description,sizeof(T[nbreTaches].description),stdin);
+      // ( T[nbreTaches].description[strcspn(description,"\n")]=\'0');
+   //scanf("%s",T[taille].description);
    printf("Entrer la date d'echeance  :\n");
-   scanf("%d %d %d",&T[taille].date.jour,&T[taille].date.mois,&T[taille].date.ann);
+   scanf("%d %d %d",&T[nbreTaches].date.jour,&T[nbreTaches].date.mois,&T[nbreTaches].date.ann);
    int choix; 
    printf("Entrer la priorite(1:pour high et 2:pour low): \n");
 //   	scanf("%d",&T[nbretache].priorite);
@@ -35,29 +42,30 @@
    	   scanf("%d",&choix);
    		//printf("Priorite: %d\n", (T[nbretache].priorite==  1) ?"high":"low");  //l'operateur ternaire
   			 	if (choix== 1) {
-  			 		T[taille].priorite=1;
+  			 		T[nbreTaches].priorite=1;
                       printf("Priorité: high\n");
                 } else if(choix== 2) {
-                	T[taille].priorite=2;
+                	T[nbreTaches].priorite=2;
                            printf("Priorité: low\n");
                      }else printf("priorite inconnue");
-
+             getchar();
 //   	   switch(choix){
 //   	case 1:strcpy(T[taille].priorite,"high");break; 
 //   	case 2:strcpy(T[taille].priorite,"low");break;
 //   	default : printf("choix introuvable ");      
 //   }
    }while(choix!=1 && choix!=2);
-     taille++;
-   if(nbretache>=50){
+     nbreTaches++;
+   if(nbretaches>=50){
    printf("Erreur: ");// limite de taches atteinte
    }
 }
 // Affichage des taches 
    void AfficherlisteTaches(){
+   	int nbreTaches;
    	printf("\n______Liste des taches______\n");
    	int i;
-   	for(i=0;i<taille;i++){
+   	for(i=0;i<nbreTaches;i++){
    		printf("Titre: %s\n ",T[i].titre);
    		printf("Description: %s \n",T[i].description);
    		//printf("Priorite: %d\n",T[nbretache].priorite);
@@ -71,40 +79,90 @@
 	   }
    }
    // Modifier les taches
-   void ModifierTache(){
+   void ModifierTache() {
    	int nbreTaches;
-   	int choix;
-   if(nbreTaches>0){
-   	int numeroTache;
-   	printf("Donner le numero de tache a modifier\n");
-   	scanf("%d",&numeroTache);
-   	if(numeroTache>0 && numeroTache<=nbreTaches){
-   	Tache* modifierTache=&T[numeroTache - 1];
-   	printf("Entrer un nouveau titre\n ");
-   	scanf("%s",modifierTache->titre);
-   	 
-   	 printf("Entrer une nouvelle description \n");
-   	 scanf("%s",modifierTache->description);
-   	 
-   	 printf("Entrer une  nouvelle date d'acheance(sous la forme j/m/ann)\n");
-   	 scanf("%d/%d/%d",&modifierTache->date.jour,&modifierTache->date.mois,&modifierTache->date.ann);
-   	 
-    	printf("Entrer une nouvelle priorite(1:pour high et 2:pour low): \n");
-    printf("Entrer une nouvelle priorité (1 pour HIGH, 2 pour LOW):\n");
-    do {
-        scanf("%d", &choix);
-        if (choix == high || choix == low) {
-            modifierTache->priorite == choix;
+    if (nbreTaches > 0) {
+        int numeroTache, choix;
+
+        // Demander le numero de la tache a modifier
+        printf("Donner le numero de tache a modifier (entre 1 et %d) :\n", nbreTaches);
+        scanf("%d",&numeroTache);
+
+        // Verifier si le numero est valide
+        if (numeroTache > 0 && numeroTache <=nbreTaches){
+		
+            Tache* modifierTache = &T[numeroTache - 1];
+
+            // Modifier le titre
+            printf("Entrer un nouveau titre :\n");
+           getchar(); // Pour consommer le '\n' laissé par scanf
+            fgets(modifierTache->titre, sizeof(modifierTache->titre), stdin);
+            // T[nbreTaches].titre[strcspn(titre,"\n")]=\'0';
+
+            // Modifier la description
+            printf("Entrer une nouvelle description :\n");
+            fgets(modifierTache->description, sizeof(modifierTache->description), stdin);
+             // T[nbreTaches].description[strcspn(description,"\n")]=\'0';
+  
+            // Modifier la date d'échéance
+            printf("Entrer une nouvelle date d'acheance (sous la forme jj/mm/aaaa) :\n");
+            scanf("%d/%d/%d", &modifierTache->date.jour, &modifierTache->date.mois, &modifierTache->date.ann);
+
+            // Modifier la prioritE
+            printf("Entrer une nouvelle priorite (1 pour HIGH, 2 pour LOW) :\n");
+            do {
+                scanf("%d", &choix);
+                if (choix == 1 || choix == 2) {
+                    modifierTache->priorite == choix;
+                } else {
+                    printf("Choix invalide, veuillez entrer 1 ou 2 :\n");
+                }
+            } while (choix == 1 ||  choix == 2); 
+             
+            printf("Tache modifiee avec succes ^-^\n");
         } else {
-            printf("Choix introuvable, veuillez reessayer.\n");
+            printf("Numero de tache invalide.\n");
         }
-    } while (choix != high && choix != low);
-//   	scanf("%d",(int*)&modifierTache->priorite); 
-//   	
-   	printf("Tache modifier avec succes ^-^");
-   }else printf("Aucune Tache a modifier ");
-}else printf("Aucune Tache a modifier");
+    } else {
+        printf("Aucune Tache a modifier.\n");
+    }
 }
+
+//   void ModifierTache(){
+//   	int nbreTaches;
+//   	int choix;
+//   if(nbreTaches>0){
+//   	int numeroTache;  // titre[strcspn (titre,"\n")]=\'0'
+//   	printf("Donner le numero de tache a modifier\n");
+//   	scanf("%d",&numeroTache);
+//   	if(numeroTache>0 && numeroTache<=nbreTaches){
+//   	Tache* modifierTache=&T[numeroTache - 1];
+//   	printf("Entrer un nouveau titre\n ");
+//   	scanf(" %[^\n]",modifierTache->titre);
+//   	 
+//   	 printf("Entrer une nouvelle description \n");
+//   	 scanf(" %[^\n]",modifierTache->description);
+//   	 
+//   	 printf("Entrer une  nouvelle date d'acheance(sous la forme j/m/ann)\n");
+//   	 scanf("%d/%d/%d",&modifierTache->date.jour,&modifierTache->date.mois,&modifierTache->date.ann);
+//   	 
+//    	printf("Entrer une nouvelle priorite(1:pour high et 2:pour low): \n");
+//    	getchar();
+//   // printf("Entrer une nouvelle priorité (1 pour HIGH, 2 pour LOW):\n");
+//    do {
+//        scanf("%d", &choix);
+//        if (choix == high || choix == low) {
+//            modifierTache->priorite == choix;
+//        } else {
+//            printf("Choix introuvable, veuillez reessayer.\n");
+//        }
+//    } while (choix != high && choix != low);
+////   	scanf("%d",(int*)&modifierTache->priorite); 
+////   	
+//   	printf("Tache modifier avec succes ^-^");
+//   }else printf("Aucune Tache a modifier ");
+//}else printf("Aucune Tache a modifier");
+//}
 void SupprimerTache(){
 	AfficherlisteTaches();
 	int nbreTaches;
@@ -150,20 +208,20 @@ void TriCroissante() {
 }
       //filtrer les tache par priorite 
      void FiltrerTache(){
-     	AfficherlisteTaches();
-     	int priorite, nbreTaches;
+     //	AfficherlisteTaches();
+     	int priorite;
      	int i;
+     	int nbreTaches;
      	printf("Filtrer  par priorite \n");
      	scanf("%d",&priorite);
      	printf("\n-----liste des tache filtrees-----");
      	for(i=0;i<nbreTaches;i++){
-     		Tache T[i];
      		if(T[i].priorite==priorite){
      			printf("Tache %d",i+1);
-     			printf("Titre: %s\n",T[i].titre);
-     			printf("Description: %s\n",T[i].description);
-     			printf("Date d'echeance:%d/%d/%d \n",T[i].date.jour,T[i].date.mois,T[i].date.ann);
-     			printf("Priorite: %d\n",(T[nbreTaches].priorite ==high) ?"high":"low");  //l'operateur ternaire
+     			printf("Titre: %s\n",T[i].titre);}}}
+//     			printf("Description: %s\n",T[i].description);
+//     			printf("Date d'echeance:%d/%d/%d \n",T[i].date.jour,T[i].date.mois,T[i].date.ann);
+//     			printf("Priorite: %d\n",(T[nbreTaches].priorite ==high) ?"high":"low");  //l'operateur ternaire
 //			 	if (T.priorite== high) {
 //                        printf("Priorité: high\n");
 //                        } else {
@@ -176,10 +234,10 @@ void TriCroissante() {
 //	printf("la priorite est normale"); break;
 //default :
 //	printf("priorite inconnue");
-}
-                        printf("\n--------------------------------\n");
-						}
-			 }
+
+                     //   printf("\n--------------------------------\n");
+						
+			 
 //		 }
 //	 }
 //   nouvelleTache.id=nbretache+1;
@@ -203,7 +261,8 @@ int main(){
 			printf("3: Modifier \n");
 				printf("4: Supprimer \n");
 					printf("5: Filtrer les taches par prioriter \n");// 
-					printf("6: Quitter \n");
+						printf("6: TriCroissante \n");
+					printf("7: Quitter \n");
 						printf("tapez votre choix: \n");
 						scanf("%d",&choix);
 				switch(choix){
@@ -218,8 +277,8 @@ int main(){
 //					scanf("%d",&tache);
 					case 5: FiltrerTache();break;
 //					Supprimer(tache); break;
-                    case 6:  TriCroissante() ; break;
-					case 7:
+                    case 6: TriCroissante() ; break;
+					case 7: 
 						printf("Au revoir! \n"); break;
 				    default : printf("choix introuvable,veuillez reessayer ^-^");
 				}
